a9a9581fd666117b6c3933ffe6aa18f6635abba2
NguyenTrinh3008 <149233420+NguyenTrinh3008@users.noreply.github.com>
2024-11-18 16:56:53 +0700

    Implement the Navie Bayes and OneR
    FIx Bug

--- src/main/java/OneRClassifier.java
+++ src/main/java/OneRClassifier.java
@@ -1,17 +1,17 @@
+import weka.classifiers.rules.OneR;
 import weka.core.Instances;
 
-public class OneRClassifier extends BaseClassifier{
+public class OneRClassifier extends BaseClassifier {
 
-    public OneRClassifier(){
+    public OneRClassifier() {
         modelName = "OneR";
     }
 
     @Override
-    public void buildClassifier(Instances trainingData) throws Exception{
-        // Implement OneR algorithm here
-        System.out.println("demo");
-        throw new UnsupportedOperationException("Not implemented yet");
+    public void buildClassifier(Instances trainingData) throws Exception {
+        classifier = new OneR(); // Initialize the OneR classifier
+        ((OneR) classifier).setOptions(new String[] {"-B", "6"}); // Align configuration with Weka
+        classifier.buildClassifier(trainingData); // Train the model
+        System.out.println("OneR model built successfully.");
     }
-
-
 }
